c3ad497d61cccc2e33f97c9f7f0fee6de291cc42
Miguel Reyes <miguelreyesba@gmail.com>
2018-04-19 10:35:00 -0500

    creando repo

---
+++ src/main/java/mx/itam/adsi/ejercicio/HolaMundo.java
@@ -0,0 +1,12 @@
+package mx.itam.adsi.ejercicio;
+
+public class HolaMundo {
+  
+    public static void hola(String s) {
+        System.out.println("Hola "+s);
+    }
+ 
+    public static void main(String... args) {
+    	hola("Amanda");
+    }
+}
